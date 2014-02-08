dca26a0c416112db0c0b42fcbe16616bb0d4bf08
Shane Kilkelly <shane@kilkelly.me>
2014-02-08 19:09:05 +0000

    test for malformed auth posts

--- test/radsticks/test/handler.clj
+++ test/radsticks/test/handler.clj
@@ -42,5 +42,80 @@
         (is (contains? response-json :token))
         (is (= (class (response-json :token)) java.lang.String))
         (is (< 0 (count (response-json :token))))
-        (is (= "userone@example.com" (response-json :username)))))))
+        (is (= "userone@example.com" (response-json :username))))
 
+      ;; malformed request, missing username
+      (let [request-body
+            "{\"derp\":\"userone@example.com\",
+              \"password\":\"password1\"}"
+            request (-> (session app)
+                        (content-type "application/json")
+                        (request "/api/auth"
+                                 :request-method :post
+                                 :body request-body))
+            response (:response request)
+            response-json (parse-string (response :body) true)]
+        (is (= "application/json;charset=UTF-8"
+               (get (:headers response) "Content-Type")))
+        (is (not (= (:status response) 201)))
+        (is (= (:status response) 400))
+        (is (not (contains? response-json :token)))
+        (is (contains? response-json :error))
+        (is (= (class (response-json :error)) java.lang.String)))
+
+      ;; malformed request, missing password
+      (let [request-body
+            "{\"username\":\"userone@example.com\",
+              \"derp\":\"password1\"}"
+            request (-> (session app)
+                        (content-type "application/json")
+                        (request "/api/auth"
+                                 :request-method :post
+                                 :body request-body))
+            response (:response request)
+            response-json (parse-string (response :body) true)]
+        (is (= "application/json;charset=UTF-8"
+               (get (:headers response) "Content-Type")))
+        (is (not (= (:status response) 201)))
+        (is (= (:status response) 400))
+        (is (not (contains? response-json :token)))
+        (is (contains? response-json :error))
+        (is (= (class (response-json :error)) java.lang.String)))
+
+      ;; malformed request, username is not string
+      (let [request-body
+            "{\"username\":[1,2,3],
+              \"password\":\"password1\"}"
+            request (-> (session app)
+                        (content-type "application/json")
+                        (request "/api/auth"
+                                 :request-method :post
+                                 :body request-body))
+            response (:response request)
+            response-json (parse-string (response :body) true)]
+        (is (= "application/json;charset=UTF-8"
+               (get (:headers response) "Content-Type")))
+        (is (not (= (:status response) 201)))
+        (is (= (:status response) 400))
+        (is (not (contains? response-json :token)))
+        (is (contains? response-json :error))
+        (is (= (class (response-json :error)) java.lang.String)))
+
+      ;; malformed request, password is not string
+      (let [request-body
+            "{\"username\":\"userone@example.com\",
+              \"password\":true}"
+            request (-> (session app)
+                        (content-type "application/json")
+                        (request "/api/auth"
+                                 :request-method :post
+                                 :body request-body))
+            response (:response request)
+            response-json (parse-string (response :body) true)]
+        (is (= "application/json;charset=UTF-8"
+               (get (:headers response) "Content-Type")))
+        (is (not (= (:status response) 201)))
+        (is (= (:status response) 400))
+        (is (not (contains? response-json :token)))
+        (is (contains? response-json :error))
+        (is (= (class (response-json :error)) java.lang.String))))))
