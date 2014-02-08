61964e031375956b514606d18effc76fea8dfe7f
Shane Kilkelly <shane@kilkelly.me>
2014-02-08 18:04:32 +0000

    api routes for authentication

--- src/radsticks/routes/api.clj
+++ src/radsticks/routes/api.clj
@@ -1,22 +1,41 @@
 (ns radsticks.routes.api
   (:use compojure.core)
-  (:require [liberator.core :refer [defresource]]))
+  (:require [liberator.core :refer [defresource]]
+            [radsticks.auth :as auth]
+            [radsticks.db :as db]))
 
 
-(def things
-  ["Clojure",
-   "Liberator",
-   "AngularJS",
-   "CoffeeScript",
-   "Sass",
-   "Grunt",
-   "Bower"])
-
-
-(defresource nice-things
+(defresource authentication
   :available-media-types ["application/json"]
-  :handle-ok {:things things})
+  :allowed-methods [:post]
+
+  :malformed?
+  (fn [context]
+    (let [params (get-in context  [:request :params])
+          username (params :username)
+          password (params :password)]
+      [(or (nil? username)
+           (nil? password)
+           (not (= (class username) java.lang.String))
+           (not (= (class password) java.lang.String)))
+       {:representation {:media-type "application/json"}}]))
+
+  :handle-malformed
+  (fn [context]
+    {:error "post malformed"})
+
+  :post!
+  (fn [context]
+    (let [params (get-in context [:request :params])
+          username (params :username)
+          password (params :password)
+          token (auth/authenticate-user username password)]
+      {:payload
+       {:username username, :token token}}))
+
+  :handle-created
+  (fn [context] (context :payload)))
 
 
 (defroutes api-routes
-  (GET "/api/nicethings" [] nice-things))
+  (POST "/api/auth" [] authentication))
