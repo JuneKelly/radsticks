7df9bc78c3cfcb5a6836e25d9b3a547e627a360a
Shane Kilkelly <shane@kilkelly.me>
2014-03-01 20:24:12 +0000

    add a basic sql script to create schema

---
+++ resources/sql/schema/create.sql
@@ -0,0 +1,21 @@
+-- Create fresh copy of radsticks schema
+
+-- User
+DROP TABLE IF EXISTS user_account;
+
+CREATE TABLE user_account (
+  id varchar(128) primary key,
+  password text not NULL,
+  name text not NULL,
+  created timestamp not NULL,
+  last_login timestamp
+);
+
+
+-- Log
+DROP TABLE IF EXISTS log;
+
+CREATE TABLE log (
+  id serial primary key,
+  data json not NULL
+)
