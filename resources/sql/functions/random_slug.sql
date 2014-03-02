497e78a74cc4950502b98163cbf1726818059cf1
Shane Kilkelly <shane@kilkelly.me>
2014-03-02 14:57:13 +0000

    add the random_slug function

---
+++ resources/sql/functions/random_slug.sql
@@ -0,0 +1,12 @@
+-- generate safe, random eight-character strings
+CREATE OR REPLACE FUNCTION random_slug()
+RETURNS text
+AS
+$$ SELECT CAST(
+     regexp_replace(
+       encode(
+         gen_random_bytes(6), 'base64'),
+         '[/=+]',
+         '-', 'g'
+   ) AS text);$$
+LANGUAGE SQL
