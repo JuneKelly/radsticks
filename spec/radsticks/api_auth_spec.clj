46bd87db137f4c4e39a803c09799b789354327ed
Shane Kilkelly <shane@kilkelly.me>
2014-03-18 18:38:46 +0000

    split up api specs

---
+++ spec/radsticks/api_auth_spec.clj
@@ -0,0 +1,145 @@
+(ns radsticks.api-auth-spec
+  (:require [radsticks.test-utils :as util]
+            [speclj.core :refer :all]
+            [peridot.core :refer :all]
+            [radsticks.handler :refer :all]
+            [radsticks.db.user :as user]
+            [cheshire.core :refer [generate-string
+                                   parse-string]]))
+
+(describe
+  "auth api"
+
+  (before
+   (do (util/reset-db!)
+       (util/populate-users!)))
+
+  (it "should issue a token when credentials are correct"
+    (let [request-body
+          "{\"email\":\"userone@example.com\",
+            \"password\":\"password1\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)
+          response-json (parse-string (response :body) true)]
+      (should (= "application/json;charset=UTF-8"
+                 (get (:headers response) "Content-Type")))
+      (should (= (:status response) 201))
+      (should (contains? response-json :token))
+      (should (string? (response-json :token)))
+      (should (< 0 (count (response-json :token))))
+      (should (= "userone@example.com" (response-json :email)))))
+
+  (it "should fail to authenticate when email is unknown"
+    (let [request-body
+          "{\"email\":\"gooser@example.com\",
+            \"password\":\"lol\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)]
+      (should (= "text/plain"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 403))
+      (should (= "Forbidden." (response :body)))))
+
+  (it "should fail to authenticate when password is incorrect"
+    (let [request-body
+          "{\"email\":\"userone@example.com\",
+            \"password\":\"lol\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)]
+      (should (= "text/plain"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 403))
+      (should (= "Forbidden." (response :body)))))
+
+  (it "should fail when user email is not submitted"
+    (let [request-body
+          "{\"derp\":\"userone@example.com\",
+            \"password\":\"password1\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)
+          response-json (parse-string (response :body) true)]
+      (should (= "application/json;charset=UTF-8"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 400))
+      (should (not (contains? response-json :token)))
+      (should (contains? response-json :errors))
+      (should (vector? (response-json :errors)))))
+
+  (it "should fail when password is not submitted"
+    (let [request-body
+          "{\"email\":\"userone@example.com\",
+            \"derp\":\"password1\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)
+          response-json (parse-string (response :body) true)]
+      (should (= "application/json;charset=UTF-8"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 400))
+      (should (not (contains? response-json :token)))
+      (should (contains? response-json :errors))
+      (should (vector? (response-json :errors)))))
+
+  (it "should fail when the supplied email is not a string"
+    (let [request-body
+          "{\"email\":[1,2,3],
+            \"password\":\"password1\"}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)
+          response-json (parse-string (response :body) true)]
+      (should (= "application/json;charset=UTF-8"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 400))
+      (should (not (contains? response-json :token)))
+      (should (contains? response-json :errors))
+      (should (vector? (response-json :errors)))))
+
+  (it "should fail when password is not a string"
+    (let [request-body
+          "{\"email\":\"userone@example.com\",
+            \"password\":true}"
+          request (-> (session app)
+                      (content-type "application/json")
+                      (request "/api/auth"
+                               :request-method :post
+                               :body request-body))
+          response (:response request)
+          response-json (parse-string (response :body) true)]
+      (should (= "application/json;charset=UTF-8"
+                 (get (:headers response) "Content-Type")))
+      (should (not (= (:status response) 201)))
+      (should (= (:status response) 400))
+      (should (not (contains? response-json :token)))
+      (should (contains? response-json :errors))
+      (should (vector? (response-json :errors))))))
+
+
+(run-specs)
--- spec/radsticks/api_user_spec.clj
+++ spec/radsticks/api_user_spec.clj
@@ -1,4 +1,4 @@
-(ns radsticks.api-spec
+(ns radsticks.api-user-spec
   (:require [radsticks.test-utils :as util]
             [speclj.core :refer :all]
             [peridot.core :refer :all]
@@ -6,22 +6,6 @@
             [radsticks.db.user :as user]
             [cheshire.core :refer [generate-string
                                    parse-string]]))
-
-
-(describe
-  "main route"
-
-  (it "should respond correctly on home route"
-    (let [response (:response
-                     (-> (session app)
-                         (request "/")))]
-      (should (= (response :status) 200))))
-
-  (it "should return 404 on unknown route"
-    (let [response (:response
-                     (-> (session app)
-                         (request "/invalid/route")))]
-      (should (= (:status response) 404)))))
 
 
 (describe
@@ -255,140 +239,4 @@
         (should-not= (old-profile :name) (response-json :name)))))
 
 
-(describe
-  "auth api"
-
-  (before
-   (do (util/reset-db!)
-       (util/populate-users!)))
-
-  (it "should issue a token when credentials are correct"
-    (let [request-body
-          "{\"email\":\"userone@example.com\",
-            \"password\":\"password1\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)
-          response-json (parse-string (response :body) true)]
-      (should (= "application/json;charset=UTF-8"
-                 (get (:headers response) "Content-Type")))
-      (should (= (:status response) 201))
-      (should (contains? response-json :token))
-      (should (string? (response-json :token)))
-      (should (< 0 (count (response-json :token))))
-      (should (= "userone@example.com" (response-json :email)))))
-
-  (it "should fail to authenticate when email is unknown"
-    (let [request-body
-          "{\"email\":\"gooser@example.com\",
-            \"password\":\"lol\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)]
-      (should (= "text/plain"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 403))
-      (should (= "Forbidden." (response :body)))))
-
-  (it "should fail to authenticate when password is incorrect"
-    (let [request-body
-          "{\"email\":\"userone@example.com\",
-            \"password\":\"lol\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)]
-      (should (= "text/plain"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 403))
-      (should (= "Forbidden." (response :body)))))
-
-  (it "should fail when user email is not submitted"
-    (let [request-body
-          "{\"derp\":\"userone@example.com\",
-            \"password\":\"password1\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)
-          response-json (parse-string (response :body) true)]
-      (should (= "application/json;charset=UTF-8"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 400))
-      (should (not (contains? response-json :token)))
-      (should (contains? response-json :errors))
-      (should (vector? (response-json :errors)))))
-
-  (it "should fail when password is not submitted"
-    (let [request-body
-          "{\"email\":\"userone@example.com\",
-            \"derp\":\"password1\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)
-          response-json (parse-string (response :body) true)]
-      (should (= "application/json;charset=UTF-8"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 400))
-      (should (not (contains? response-json :token)))
-      (should (contains? response-json :errors))
-      (should (vector? (response-json :errors)))))
-
-  (it "should fail when the supplied email is not a string"
-    (let [request-body
-          "{\"email\":[1,2,3],
-            \"password\":\"password1\"}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)
-          response-json (parse-string (response :body) true)]
-      (should (= "application/json;charset=UTF-8"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 400))
-      (should (not (contains? response-json :token)))
-      (should (contains? response-json :errors))
-      (should (vector? (response-json :errors)))))
-
-  (it "should fail when password is not a string"
-    (let [request-body
-          "{\"email\":\"userone@example.com\",
-            \"password\":true}"
-          request (-> (session app)
-                      (content-type "application/json")
-                      (request "/api/auth"
-                               :request-method :post
-                               :body request-body))
-          response (:response request)
-          response-json (parse-string (response :body) true)]
-      (should (= "application/json;charset=UTF-8"
-                 (get (:headers response) "Content-Type")))
-      (should (not (= (:status response) 201)))
-      (should (= (:status response) 400))
-      (should (not (contains? response-json :token)))
-      (should (contains? response-json :errors))
-      (should (vector? (response-json :errors))))))
-
-
 (run-specs)
-
