ea6738ff4c6b5a6a15891b248e20109a6730336a
Shane Kilkelly <shane@kilkelly.me>
2014-03-25 18:34:02 +0000

    upgrade to clojure 1.6.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
   :url "http://github.com/ShaneKilkelly/radsticks"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [lib-noir "0.8.1"]
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
