a8a8eb523201ac763e6514ee281466d6351e227a
Shane Kilkelly <shane@kilkelly.me>
2014-04-01 18:55:45 +0100

    add validation functions for the user resource, not in use currently, need to refactor user api to make use of these

--- src/radsticks/validation.clj
+++ src/radsticks/validation.clj
@@ -19,3 +19,24 @@
                   (presence-of :content)
                   (presence-of :tags))]
     (validate data)))
+
+
+(def email-regex
+  #"(?i)[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]")
+
+
+(defn get-user-creation-errors [data]
+  (let [validate (validation-set
+                  (presence-of :email)
+                  (format-of :email
+                             :format email-regex
+                             :message "must be an email address")
+                  (presence-of :password)
+                  (presence-of :name))]
+    (validate data)))
+
+
+(defn get-user-update-errors [data]
+  (let [validate (validation-set
+                  (presence-of :name))]
+    (validate data)))
